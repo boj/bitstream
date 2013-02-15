0e92f52c0117d5a2670259c95981fb5d62f5bc73
Brian Jones <mojobojo@gmail.com>
2013-02-15 21:39:10 +0900

    documentation

--- README.md
+++ README.md
@@ -2,98 +2,14 @@
 
 A simple library for sending byte data across the wire.
 
+## Install
+
+    go get github.com/boj/bitstream
+
 ## Usage
 
-A message gets constructed like the following:
-
-    [total message length]  - 4 bytes
-      [byte][value]         - 1 byte + 1 byte
-      [int32][value]        - 1 byte + 4 bytes
-      [float32][value]      - 1 byte + 4 bytes
-      [bool (int8)][value]  - 1 byte + 1 byte
-      [string][size][value] - 1 byte + 4 bytes + N bytes
-      ...
-    
-Message reading and writing is completely user driven.
-
-The network loop handles reading the first 4 bytes to determine the message size, and passes the rest of the data to the BitStream.Read() method.
-
-Example:
-
-    // Sender
-    bs := NewBitStream()
-    bs.WriteByte(messageType)
-    bs.WriteInt(objectId)
-    bs.WriteFloat32(position.x)
-    bs.WriteFloat32(position.y)
-    bs.WriteFloat32(position.z)
-    msg := bs.Write()
-
-    // Network loop reads first 4 bytes, passes rest of data to MessageHandler()
-  
-    // Receiver
-    func MessageHandler(msg []byte) error {
-      bs := NewBitStream()
-      if err := bs.Read(msg); err != nil {
-        return err
-      } else {  
-        if msgType, err := bs.ReadByte(); err != nil {
-          return err
-        } else {
-          switch msgType {
-            case ENTITY_MOVED:
-              // read id
-              id, err := bs.ReadInt()
-              if err != nil {
-                return err
-              }
-              e := entity[id]
-              // read position
-              x, err := bs.ReadFloat()
-              if err != nil {
-                return err
-              }
-              y, err := bs.ReadFloat()
-              if err != nil {
-                return err
-              }
-              z, err := bs.ReadFloat()
-              if err != nil {
-                return err
-              }
-              e.SetPosition(x, y, z)
-              break
-          }
-        }
-      }
-    }
-  
-This library assumes the reciever understands the order the message parts are received in.
+See the inline source docs, or view them with godoc.
 
 ## Author
 
 Brian 'bojo' Jones <mojobojo@gmail.com>
-
-## License
-
-The MIT License
- 
-Copyright (c) 2012 Brian Jones
- 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
- 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
- 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
--- bitstream.go
+++ bitstream.go
@@ -1,3 +1,77 @@
+// Copyright 2013 Brian 'bojo' Jones. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+/*
+Package bitstream is a simple library for sending byte data across the wire.
+
+Bitstream messages are built using the following guidelines for primitive types:
+
+    [total message length]  - 4 bytes
+      [byte][value]         - 1 byte + 1 byte
+      [int32][value]        - 1 byte + 4 bytes
+      [float32][value]      - 1 byte + 4 bytes
+      [bool (byte)][value]  - 1 byte + 1 byte
+      [string][size][value] - 1 byte + 4 bytes + N bytes
+      ...
+
+Message reading and writing is completely user driven.
+
+The user defined network loop handles reading the first 4 bytes to determine the message size.
+The following data should then be passed to the instantiated Read() method.
+
+Example:
+
+    // Sender
+    bs := NewBitStream()
+    bs.WriteByte(messageType)
+    bs.WriteInt(objectId)
+    bs.WriteFloat(position.x)
+    bs.WriteFloat(position.y)
+    bs.WriteFloat(position.z)
+    msg := bs.Write()
+
+    // Network loop reads first 4 bytes, passes rest of data to MessageHandler()
+
+    // Receiver
+    func MessageHandler(msg []byte) error {
+      bs := NewBitStream()
+      if err := bs.Read(msg); err != nil {
+        return err
+      } else {  
+        if msgType, err := bs.ReadByte(); err != nil {
+          return err
+        } else {
+          switch msgType {
+            case ENTITY_MOVED:
+              // read id
+              id, err := bs.ReadInt()
+              if err != nil {
+                return err
+              }
+              e := entity[id]
+              // read position
+              x, err := bs.ReadFloat()
+              if err != nil {
+                return err
+              }
+              y, err := bs.ReadFloat()
+              if err != nil {
+                return err
+              }
+              z, err := bs.ReadFloat()
+              if err != nil {
+                return err
+              }
+              e.SetPosition(x, y, z)
+              break
+          }
+        }
+      }
+    }
+
+This library assumes the reciever understands the order the message parts are received in.
+*/
 package bitstream
 
 import (
@@ -18,19 +92,26 @@ type BitStream struct {
 	data bytes.Buffer
 }
 
+// NewBitStream creates a new bitstream.
 func NewBitStream() *BitStream {
 	self := new(BitStream)
 	return self
 }
 
+// Length returns the length of the byte data contained in the bitstream.
+// This does not include the first 4 bytes read by the network loop.
 func (self *BitStream) Length() int {
 	return self.data.Len()
 }
 
+// Reset resets the state of the bitstream to nothing.
 func (self *BitStream) Reset() {
 	self.data.Reset()
 }
 
+// Read takes in a stream of bytes read in from the network, not including
+// the initial 4 bytes read by the network loop.  The library can then
+// parse and return the results.
 func (self *BitStream) Read(b []byte) error {
 	if len(b) == 0 {
 		return errors.New("BitStream: passed 0 length message to Read()\n")
@@ -39,6 +120,7 @@ func (self *BitStream) Read(b []byte) error {
 	return nil
 }
 
+// ReadByte returns a byte.
 func (self *BitStream) ReadByte() (byte, error) {
 	var check []byte = self.data.Next(1)
 	if check[0] != TYPE_BYTE {
@@ -48,6 +130,7 @@ func (self *BitStream) ReadByte() (byte, error) {
 	return buffer[0], nil
 }
 
+// ReadInt returns an int32.
 func (self *BitStream) ReadInt() (int32, error) {
 	var check []byte = self.data.Next(1)
 	if check[0] != TYPE_INT {
@@ -59,6 +142,7 @@ func (self *BitStream) ReadInt() (int32, error) {
 	return ret, nil
 }
 
+// ReadFloat returns a float32.
 func (self *BitStream) ReadFloat() (float32, error) {
 	var check []byte = self.data.Next(1)
 	if check[0] != TYPE_FLOAT {
@@ -71,6 +155,7 @@ func (self *BitStream) ReadFloat() (float32, error) {
 
 }
 
+// ReadBool returns a bool.
 func (self *BitStream) ReadBool() (bool, error) {
 	var check []byte = self.data.Next(1)
 	if check[0] != TYPE_BOOL {
@@ -83,6 +168,7 @@ func (self *BitStream) ReadBool() (bool, error) {
 	return false, nil
 }
 
+// ReadString returns a string.
 func (self *BitStream) ReadString() (string, error) {
 	var check []byte = self.data.Next(1)
 	if check[0] != TYPE_STRING {
@@ -95,11 +181,13 @@ func (self *BitStream) ReadString() (string, error) {
 	return string(ret), nil
 }
 
+// WriteByte writes a byte to the bitstream.
 func (self *BitStream) WriteByte(b byte) {
 	self.data.WriteByte(TYPE_BYTE)
 	self.data.WriteByte(b)
 }
 
+// WriteInt writes a in32 to the bitstream.
 func (self *BitStream) WriteInt(i int32) {
 	self.data.WriteByte(TYPE_INT)
 	tmp := new(bytes.Buffer)
@@ -107,6 +195,7 @@ func (self *BitStream) WriteInt(i int32) {
 	self.data.Write(tmp.Bytes())
 }
 
+// WriteFloat writes a float32 to the bitstream.
 func (self *BitStream) WriteFloat(f float32) {
 	self.data.WriteByte(TYPE_FLOAT)
 	tmp := new(bytes.Buffer)
@@ -114,6 +203,7 @@ func (self *BitStream) WriteFloat(f float32) {
 	self.data.Write(tmp.Bytes())
 }
 
+// WriteBool writes a bool to the bitstream.
 func (self *BitStream) WriteBool(b bool) {
 	self.data.WriteByte(TYPE_BOOL)
 	if b {
@@ -123,6 +213,7 @@ func (self *BitStream) WriteBool(b bool) {
 	}
 }
 
+// WriteString writes a string to the bitstream.
 func (self *BitStream) WriteString(s string) {
 	self.data.WriteByte(TYPE_STRING)
 	size := new(bytes.Buffer)
@@ -131,6 +222,7 @@ func (self *BitStream) WriteString(s string) {
 	self.data.WriteString(s)
 }
 
+// Write returns the bitstream data as a byte array.
 func (self *BitStream) Write() []byte {
 	// write message size
 	b := new(bytes.Buffer)
